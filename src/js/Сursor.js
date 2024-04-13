ec9465dc547facd7f26bcb68509b81cf8a55482c
RoziznanVadim <roziznan_v@mail.ru>
2024-04-13 15:21:26 +0300

    in progress

--- src/js/Сursor.js
+++
@@ -1,9 +0,0 @@
-export default class Cursor {
-  constructor(element) {
-    this.element = element;
-  }
-  changeCursor() {
-    document.querySelector('.field').style.cursor = 'url("../images/goblin.png"), auto';
-  }
-  
-}
